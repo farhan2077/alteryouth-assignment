38d8ca88b26522d5e5c3aa2a60947619f31f8207
farhanbinamin@gmail.com <farhanbinamin@gmail.com>
2023-05-16 21:22:09 +0600

    new: added tailwindcss with local font

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        "gilroy-bold": ["gilroy-bold", "sans-serif"],
+        "gilroy-medium": ["gilroy-medium", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
